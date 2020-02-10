a0db0e5524ba94d65682a634f70fd395e06c5b29
Tetsuya Takatsuru <tetsuya@taka2ru.jp>
2020-02-10 10:32:24 +0900

    cargo make

---
+++ README.md
@@ -0,0 +1,12 @@
+# hello-wasm
+
+## Overview
+
+...
+
+## Build
+
+```
+cargo install cargo-make
+cargo make build
+```
--- build.bat
+++
@@ -1,2 +0,0 @@
-cargo build --target wasm32-unknown-unknown --release
-wasm-bindgen target\wasm32-unknown-unknown\release\hello_wasm.wasm --out-dir .\pkg --target web
